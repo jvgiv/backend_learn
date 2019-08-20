4d9925bfa93d165fdabd66f7ace6645a0bddcbde
jvgiv <jvgiv12@gmail.com>
2019-08-19 21:19:13 -0400

    Final server tweaks in order

--- api/server.js
+++ api/server.js
@@ -3,3 +3,24 @@ const applyMiddleware = require('./middleware.js')
 const graphqlHTTP = require('express-graphql')
 const schema = require('../schema/schema.js')
 const expressPlayground = require('graphql-playground-middleware-express')
+    .default;
+const server = express()
+
+applyMiddleware(server)
+
+server.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+
+
+server.get('/', (req, res) => {
+    res.send("The server is running.")
+})
+
+server.use(
+    '/graphql', 
+    graphqlHTTP({
+        schema,
+        graphiql: false
+    })
+)
+
+module.exports = server
---
+++ knexfile.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const pg = require('pg')
+pg.defaults.ssl = true
+
+
+module.exports = {
+    development: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: './database/migrations',
+            tablename: 'knex_migrations'
+        },
+        seeds: {
+            directory: './database/seeds'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        useNullAsDefault: true
+    }
+}
